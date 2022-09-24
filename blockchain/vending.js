35089561f02e85bae975a5ec41685febf677eaca
yinks002 <olaiyaolayimikai@gmail.com>
2022-09-24 13:36:59 +0100

    first commit

---
+++ blockchain/vending.js
@@ -0,0 +1,14 @@
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+
+
+
+const vendingMachineContract = web3 =>{
+  return new web3.eth.Contract(
+    abi,
+    "0x521a117E1bFe69695B01657708eeB9E919607398"
+
+   );
+}
+
+export default vendingMachineContract;
